10c284adff1ecda8ff1a95bf792a45237b422ee6
fojoWork <foxyovich@gmail.com>
2026-07-14 01:17:12 -0700

    Simplified using a bitset

--- main.cpp
+++ main.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-
-
-
-
+#include <bitset>
 
 int main() {
-  int target = 70; //4 bytes
-  bool first; //1 byte
-  int cur_total = 0;
-  std::vector<int> byte_sizes = {128,64,32,16,8,4,2,1}; //32 bytes
-  std::vector<int> saved_indices(8,0); //32 bytes
-  
-  //temp
-  for (size_t i{}; i < byte_sizes.size(); ++i) {
-    if (cur_total + byte_sizes[i] <= target) {
-      saved_indices[i] = 1;
-      cur_total += byte_sizes[i];
-    }
-  }
-  
-  std::cout << target << "\n";
-  for (auto i : saved_indices) {
-    std::cout << i;
-  }
-  std::cout << "\n";
-  std:: cout << static_cast<char>(target) << "\n";
+  int x;
+  std::cout << "Please enter a decimal to convert: ";
+  std::cin >> x;
 
+  if (x < 0 || x > 255) {
+    std::cout << "Please enter a number between 0 and 255." << std::endl;
+    return 1;
+  }
+
+  std::bitset<8> bitset(x);
+  std::cout << bitset << std::endl;
+  std::cout << x;
 }
-  
-
-   
